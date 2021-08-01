5c07478cd0d7e79dd15069d05ebd3b4cc1c9c914
sanid <sanid.2001@gmail.com>
2021-08-01 12:30:53 +0530

    some changes

--- fs_wcat/nonemptylines.js
+++ fs_wcat/nonemptylines.js
@@ -2,8 +2,8 @@ let answer = [];
 function numberNonEmpty(content) {
   let count = 1;
   for (let i = 0; i < content.length; i++) {
-    if (content[i] == "") {
-      answer[i] = count++;
+    if (content[i] != "") {
+      answer[i] = count++ + " " + content[i];
     } else {
       answer[i] = content[i];
     }
